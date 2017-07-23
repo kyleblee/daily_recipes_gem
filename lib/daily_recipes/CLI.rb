64460ff3cb5dd45ca8f5300c117da1dba66c529f
kyleblee <hello.kylelee@gmail.com>
2017-07-23 10:10:27 -0400

    adding until loop to enable a 'didn't understand' message for unexpected inputs

--- lib/daily_recipes/CLI.rb
+++ lib/daily_recipes/CLI.rb
@@ -2,13 +2,17 @@ class DailyRecipes::CLI
 
   def greeting
     puts "Welcome to Daily Recipes! Would you like to see the recipes of the day? (yes / no)"
-    input = gets.strip.downcase
-    if input == "yes"
-      menu
-    elsif input == "no"
-      goodbye
-    else
-      greeting
+
+    input = nil
+    until input == "yes" || input == "no"
+      input = gets.strip.downcase
+      if input == "yes"
+        menu
+      elsif input == "no"
+        goodbye
+      else
+        puts "Sorry, not sure what you mean by that... Type 'yes' to see the menu of the day or 'no' to exit."
+      end
     end
   end
 
