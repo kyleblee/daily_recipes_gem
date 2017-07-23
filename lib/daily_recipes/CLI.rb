09bd37c3bc39927a30a699f2ae3f0c0dee201314
kyleblee <hello.kylelee@gmail.com>
2017-07-22 21:42:44 -0400

    adding conditional statements inside #greeting to determine menu, exiting the program, or calling #greeting again

--- lib/daily_recipes/CLI.rb
+++ lib/daily_recipes/CLI.rb
@@ -3,11 +3,21 @@ class DailyRecipes::CLI
   def greeting
     puts "Welcome to Daily Recipes! Would you like to see the recipes of the day? (yes / no)"
     input = gets.strip.downcase
-    menu if input == "yes"
+    if input == "yes"
+      menu
+    elsif input == "no"
+      goodbye
+    else
+      greeting
+    end
   end
 
   def menu
     puts "made it!"
+  end
+
+  def goodbye
+    puts "Thank you for using Daily Recipes! Hope to see you again soon for more delicious dishes."
   end
 
 end
